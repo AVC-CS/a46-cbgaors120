50edff9b433bb5346f3290b7313971841087002c
Kyu Lee <kwooler@gmail.com>
2026-03-05 20:41:29 -0800

    Student starter code

--- main.cpp
+++ main.cpp
@@ -10,44 +10,22 @@ int main()
     double scholarship = 10000.0;
 
     // TODO
-    cin >> status >> gender >> major;
 
     // TODO
-    if (status == "student") {
         // TODO
-        if (gender == "female") {
             // TODO
-            if (major == "cs") {
                 // TODO
-                factor = 3.0;
-            } else {
                 // TODO
-                factor = 1.0;
-            }
-        } else {
             // TODO
-            if (major == "cs") {
                 // TODO
-                factor = 2.0;
-            } else {
                 // TODO
-                factor = 0.2;
-            }
-        }
-    } else {
         // TODO
-        if (major == "cs") {
             // TODO
-            factor = 0.5;
-        }
-    }
 
     // TODO
-    double finalScholarship = scholarship * factor;
 
     cout << setprecision(2) << fixed;
     // TODO
-    cout << "Decided Factor: " << factor << ", Final Scholarship Amount: " << finalScholarship << endl;
 
     return 0;
 }
