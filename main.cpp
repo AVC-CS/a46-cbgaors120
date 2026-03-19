d41426913a250ac5e8fd3eabf1c9d7317f2f1eea
cbgaors120 <bgaor@students.avc.edu>
2026-03-19 14:36:16 -0700

    A46 Assignment

--- main.cpp
+++ main.cpp
@@ -8,23 +8,85 @@ int main()
     string status, gender, major;
     double factor = 0.1;
     double scholarship = 10000.0;
+    double finScholarship;
+    bool isStudent, isCS, isFemale;
 
-    // TODO
 
-    // TODO
-        // TODO
-            // TODO
-                // TODO
-                // TODO
-            // TODO
-                // TODO
-                // TODO
-        // TODO
-            // TODO
+    //PROMPT
+    cout << "Input letter for your answer\n";
+    cout << "Enter STATUS: \n" << "[A] Student\n" << "[B] Other\n";
+    cin >> status;
 
-    // TODO
+    cout << "Enter GENDER:\n" << "[C] Female\n" << "[D] Male\n" << "[E] Other\n";
+    cin >> gender;
+
+    cout << "Enter MAJOR:\n" << "[F] CS\n" << "[G] Other\n";
+    cin >> major;
+
+    //Convert prompt to boolean
+
+    if (status == "A"){
+        isStudent = true;
+    }
+    else {
+        isStudent = false;
+    }
+    
+    if (gender == "C"){
+        isFemale = true;
+    }
+    else{
+        isFemale = false;
+    }
+    
+    if (major == "F"){
+        isCS = true;
+    }
+    else{
+        isCS = false;
+    }
+    
+    // cout << isStudent << endl << isFemale << endl << isCS << endl;
+
+    
+    //boolean logic here
+    
+    if (!isStudent){
+
+        if (isCS)
+        factor = 0.5;
+        
+        if (!isCS)
+        factor = 0.1;       
+    }
+    if (isStudent){
+
+        if (!isCS){
+            if (!isFemale)
+            factor = 0.2;
+            if (isFemale)
+            factor = 1.0;
+        }
+        
+        if (isCS){
+            if (!isFemale)
+            factor = 2.0;
+            if (isFemale)
+            factor = 3.0;
+
+        }
+
+    }
+
+    // cout << factor;
+    
+
+    //math operations here
+    finScholarship = scholarship * factor;
 
     cout << setprecision(2) << fixed;
+    cout << "You've inputted " << status << " " << gender << " and " << major << "." << endl;
+    cout << "Your Scholarship is $" << finScholarship << endl;
     // TODO
 
     return 0;
